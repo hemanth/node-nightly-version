28374da02c732a742e7ed9fff6c1ad7bad69653e
Bhavesh Gohel <ibhaveshgohel@gmail.com>
2016-08-22 11:25:21 +0530

    Fixes #4 (#5)
    * Fixes #4
    
    Modified 'osVerMap' to work with Windows and Linux(x64 & x86). Sorry No ARM support yet!
    
    * Changes to make 'codeclimate' happy!

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const fetch = require('isomorphic-fetch');
 const nightlyVersions = require('node-nightly-versions');
 const osArch = `${process.platform}-${process.arch}`;
 const osVerMap = {
-  'win-x64':'win-x64-exe',
-  'win-x86':'win-x86-exe',
+  'win32-x64':'win-x64-msi',
+  'win32-x86':'win-x86-msi',
   'darwin-x64':'os-x64-pkg',//darwin is os name for osx
-  'darwin-x86':'os-x86-tar'
+  'darwin-x86':'os-x86-tar',
+  'linux-x64':'linux-x64',
+  'linux-x86':'linux-x86',
+  'sunos-x64':'sunos-x64',
+  'sunos-x86':'sunos-x86'
 }
 
 module.exports = () => nightlyVersions()
-  .then(versions => (versions[osArch] || versions[winVerMap[osArch]]).version);
+  .then(versions => (versions[osArch] || versions[osVerMap[osArch]]).version);
