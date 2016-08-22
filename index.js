d3f72c4e465ec7933d28dabce9652a55b174e970
Bhavesh Gohel <ibhaveshgohel@gmail.com>
2016-08-22 16:26:09 +0530

    Fix the typo! (#6)

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const osArch = `${process.platform}-${process.arch}`;
 const osVerMap = {
   'win32-x64':'win-x64-msi',
   'win32-x86':'win-x86-msi',
-  'darwin-x64':'os-x64-pkg',//darwin is os name for osx
-  'darwin-x86':'os-x86-tar',
+  'darwin-x64':'osx-x64-pkg',//darwin is os name for osx
+  'darwin-x86':'osx-x86-tar',
   'linux-x64':'linux-x64',
   'linux-x86':'linux-x86',
   'sunos-x64':'sunos-x64',
