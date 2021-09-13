cfff4d19762e998bf6cfe6549c727eecce17c121
dengruoqi <dengruoqi@enn.cn>
2021-09-13 14:59:47 +0800

    feat: support darwin-arm64(e.g. Apple Silicon)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const osVerMap = {
   'win32-x64':'win-x64-msi',
   'win32-ia32':'win-x86-msi',
   'darwin-x64':'osx-x64-pkg',//darwin is os name for osx
+  'darwin-arm64':'osx-x64-pkg',
   'darwin-ia32':'osx-x86-tar',
   'linux-x64':'linux-x64',
   'linux-ia32':'linux-x86',
